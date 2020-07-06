1b180a0888ddf8f15a61ffc393f7bd60d7c8d5bc
Micha <laughingrice@gmail.com>
2020-07-06 01:40:20 -0400

    Change default app directory to Documents/TimeLog
    Makes the timesheet more accessible and should be more platform agnostic

--- Timelog/config.py
+++ Timelog/config.py
@@ -1,6 +1,6 @@
 import pathlib
 
-app_path = (pathlib.Path().home() / pathlib.Path(".local/share/TimeLog"))
+app_path = (pathlib.Path().home() / pathlib.Path("Documents/TimeLog"))
 
 # Make sure the app directory exists
 app_path.mkdir(parents=True, exist_ok=True)
